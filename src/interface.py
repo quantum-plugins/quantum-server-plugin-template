7d11e52a8d73f98e403f379a696c297c8d4febd1
Dpbm <dpbm136@gmail.com>
2025-02-07 15:39:16 -0300

    base plugin

---
+++ src/interface.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from typing import List, Dict, Any
+
+Backend = str
+Backends = List[Backend] 
+Metadata = Dict[Any, Any]
+ResultType = str
+QasmFilePath = str
+Results = Dict|float
+
+def check_backend(func):
+    def wrapper(self, 
+    target_backend:Backend, 
+    qasm_file_path:QasmFilePath,
+    metadata:Metadata, 
+    result_type:ResultType):
+        assert target_backend in self.backends, "Invalid Target Backend. Selected Backend doesn't exists for this plugin!"
+
+        return func(self, target_backend, qasm_file_path, metadata, result_type)
+
+    return wrapper
+
+class PluginInterface(ABC):
+    def __init__(self, backends:Backends):
+        self.backends = backends
+
+    @abstractmethod
+    def execute(self, target_backend:Backend, qasm_file_path:QasmFilePath, metadata:Metadata, result_type:ResultType) -> Results:
+        pass
