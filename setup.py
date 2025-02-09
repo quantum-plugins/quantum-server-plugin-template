f39922b2efae06dc7d9d9a2ca18195296dfdeb69
Dpbm <dpbm136@gmail.com>
2025-02-09 11:34:22 -0300

    added tests

--- setup.py
+++ setup.py
@@ -26,14 +26,14 @@ def get_long_description():
 
 
 setup(
-    name="test",  # your plugin name
-    version="0.1.0",  # your plugin version
+    name="",  # your plugin name
+    version="",  # your plugin version
     install_requires=get_requirements(),
     packages=find_packages(
         # depending on how you have architected your plugin you may need to update the packages
         # for more info, take a look at:
         # https://setuptools.pypa.io/en/latest/userguide/quickstart.html#package-discovery
-        exclude=["[your plugin name].tests"]
+        exclude=["tests"]
     ),
     classifiers=[
         "Programming Language :: Python",
---
+++ tests/test_your_code.py
@@ -0,0 +1,11 @@
+class TestYourCode:
+    """
+    Add tests to your in either this file or other files in
+    this very directory.
+
+    Remember to add good names for your test files, methods and
+    classes.
+    """
+
+    def test_example(self):
+        assert 2 == 1
